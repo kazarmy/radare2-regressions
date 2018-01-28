67ee198c66ac8c6a6ef7fcfa0d599c5ef1a24a5c
Khairul Kasmiran <kazarmy@gmail.com>
2018-01-28 23:16:11 +0800

    tests.sh: output base64 in one line when dumping tests

--- tests.sh
+++ tests.sh
@@ -83,16 +83,19 @@ dump_test() {
     echo "BROKEN=1"
   fi
   printf "EXPECT64="
-  echo "$EXPECT" | base64
+  echo "$EXPECT" | base64 -w 0
+  echo
   if [ -n "$EXPECT_ERR" ]; then
     printf "EXPECT_ERR64="
-    echo "$EXPECT_ERR" | base64
+    echo "$EXPECT_ERR" | base64 -w 0
+    echo
   fi
   if [ -n "$ARGS" ]; then
     echo "ARGS=$ARGS"
   fi
   printf "CMDS64="
-  echo "$CMDS" | base64
+  echo "$CMDS" | base64 -w 0
+  echo
   echo "RUN"
   test_reset
 }
